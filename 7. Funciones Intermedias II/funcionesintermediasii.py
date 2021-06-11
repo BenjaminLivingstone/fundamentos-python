a756eaa76d36b73f027b1ed377a2f344ffd7d590
Benjamin Livingstone <you@example.com>
2021-06-10 21:37:50 -0400

    Funciones Intermedias II

---
+++ 7. Funciones Intermedias II/funcionesintermediasii.py
@@ -0,0 +1,150 @@
+# Nota: Evita usar palabras clave de clase como int, str, list y dict como nombres de variables / parámetros.
+
+# Actualiza los valores en diccionarios y listas
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# Cambia el valor 10 en x a 15. Una vez que haya terminado, x ahora debería ser [[5,2,3], [15,8,9]].
+
+def cambiaelvalor(x):
+    for i in range (0, len(x)):
+        for j in range (0,len(x[i])):
+            # print(i,x[i][j])
+            if x[i][j]==10:
+                x[i][j]=15
+    # x[1][0]=15, se puede hacer así pero preferí agregarle que cualquier 10 dentro de la lista se cambia por 15.
+    return x
+x = [[5,2,3],[10,8,9]] 
+print(cambiaelvalor(x))
+
+# Cambia el apellido del primer alumno de 'Jordan' a 'Bryant'
+
+def cambiaelapellido(students):
+    for student in students:
+        if student['last_name']=="Jordan":
+            student['last_name']="Bryant"
+    return students
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+print(cambiaelapellido(students))
+
+# En el directorio sports_directory, cambia 'Messi' a 'Andres'
+
+def cambiamessi(sports_directory):
+    for sport in sports_directory:
+        for apellido in sports_directory[sport]:
+            for x in range(0,len(sports_directory[sport])):
+                if sports_directory[sport][x]=="Messi":
+                    sports_directory[sport][x]="Andres"
+    return (sports_directory)
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+print(cambiamessi(sports_directory))
+
+# Camba el valor 20 en z a 30
+
+def cambiavalor(z):
+    # print(z)
+    for value in z:
+        for num in value:
+            if value[num]==20:
+                value[num]=30
+    return z
+z = [ {'x': 10, 'y': 20} ]
+print(cambiavalor(z))
+
+# Itera a través de una lista de diccionarios
+# Crea una función iterateDictionary(some_list)que, dada una lista de diccionarios, la función recorra cada diccionario de la lista e imprime cada clave y el valor asociado. Por ejemplo, dada la siguiente lista:
+
+def iterateDictionary(students):
+    for persona in students:
+        # print(persona.keys(),persona.values())
+        for dato in persona:
+            print(dato, persona[dato])
+        # print(persona['first_name'], persona['last_name'] )
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students)
+
+# # La salida debería ser: (Está bien si cada clave y valor quedan en dos líneas separadas)
+# # Bonus: Hacer que aparezcan exactamente así!
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+# Obtén valores de una lista de diccionarios
+# Crea una función iterateDictionary2(key_name, some_list)que, dada una lista de diccionarios y un nombre de clave, la función imprima el valor almacenado en esa clave para cada diccionario. Por ejemplo, iterateDictionary2 ('first_name', students) debería generar:
+def iterateDictionary2(info, students=[]):
+    for persona in students:
+            print(persona[info])
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# iterateDictionary2 ('first_name', students)
+iterateDictionary2('first_name', students)
+
+# Michael
+# John
+# Mark
+# KB
+# Y iterateDictionary2('last_name', students) debería generar:
+
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+# Itera a través de un diccionario con valores de listas
+
+# Crea una función printInfo(some_dict)que, dado un diccionario cuyos valores son todas listas, imprima el nombre de cada clave junto con el tamaño de su lista, y luego imprima los valores asociados dentro de la lista de cada clave. Por ejemplo:
+def printInfo(dojo):
+    for datos in dojo:
+        counter=0
+        for info in dojo[datos]:
+            counter=counter+1
+        print(counter, datos.upper())
+        for info in dojo[datos]:
+            print(info)
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
---
+++ 8. Underscore/underscore.py
@@ -0,0 +1,24 @@
+from intertools import filterfalse
+
+class Underscore:
+    def map(self, iterable, callback):
+        return
+    # def find(self, iterable, callback):
+    #     # tu código aqui
+    #     return "HOLAfind"
+    # def filter(self, iterable, callback):
+    #     # tu código aqui
+    #     return "HOLAfilter"
+    # def reject(self, iterable, callback):
+    #     # tu código 
+    #     return "HOLAreject"
+# has creado una libreria con 4 métodos
+# se crea la instancia de la clse
+_ = Underscore() # sí, estamos configurando una instancia a una variable que es un guión bajo
+evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
+# debe retornar [2, 4, 6] después que termines de implementar el código de arriba
+
+print(_.map([1,2,3], lambda x: x*2)) # debe retornar [2,4,6]
+print(_.find([1,2,3,4,5,6], lambda x: x>4)) # debe retornar el primer valor que es mayor que 4
+print(_.filter([1,2,3,4,5,6], lambda x: x%2==0)) # debe retornar [2,4,6]
+print(_.reject([1,2,3,4,5,6], lambda x: x%2!=0)) # debe retornar [1,3,5]
